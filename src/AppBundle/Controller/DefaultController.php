80f95c7e86b3cae6e528da911547360c48cc4914
Jackie Xu <jackie@jackiexu.com>
2015-09-10 15:02:29 +0200

    Notifications

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -119,11 +119,11 @@ class DefaultController extends BaseController
 
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
                 curl_setopt($ch, CURLOPT_URL, "https://android.googleapis.com/gcm/send");
+                curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
                 curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-                error_log(json_encode($data));
                 $response = curl_exec($ch);
                 curl_close($ch);
 
