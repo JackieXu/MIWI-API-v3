7a429ae01cb4c5549561adf357e89461146b8b79
Jackie Xu <jackie@jackiexu.com>
2015-10-12 16:52:57 +0200

    Item

--- src/AppBundle/Controller/ItemController.php
+++ src/AppBundle/Controller/ItemController.php
@@ -229,7 +229,6 @@ class ItemController extends BaseController
         $title = $itemValidator->getValue('title');
         $body = $itemValidator->getValue('body');
         $images = explode(',', $itemValidator->getValue('images'));
-        error_log($images);
 
         $accessManager = $this->get('manager.access');
         $accessToken = $tokenValidator->getValue('accessToken');
