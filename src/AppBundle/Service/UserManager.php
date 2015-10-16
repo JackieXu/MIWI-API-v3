c8aada384198e8a21de6ba14098f0cc9b42319b2
Jackie Xu <jackie@jackiexu.com>
2015-10-16 12:56:32 +0200

    Bug fixes

--- src/AppBundle/Service/UserManager.php
+++ src/AppBundle/Service/UserManager.php
@@ -465,12 +465,10 @@ class UserManager extends BaseManager
             $settings = $settings[0];
 
             return array(
-                'emailMentions' => $settings['emailMentions'] ? $settings['emailMentions'] : false,
                 'emailVotes' => $settings['emailVotes'] ? $settings['emailVotes'] : false,
                 'emailComments' => $settings['emailComments'] ? $settings['emailComments'] : false,
-                'appMentions' => $settings['appMentions'] ? $settings['appMentions'] : false,
                 'appVotes' => $settings['appVotes'] ? $settings['appVotes'] : false,
-                'appComments' => $settings['appComments'] ? $settings['appComments'] : false,
+                'appComments' => $settings['appComments'] ? $settings['appComments'] : false
             );
         }
 
