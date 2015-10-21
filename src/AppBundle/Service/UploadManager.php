6ed9bb1346ab79777ea7f34fee557efb197eee3b
Jackie Xu <jackie@jackiexu.com>
2015-10-21 14:36:38 +0200

    Bug fixes

--- src/AppBundle/Service/UploadManager.php
+++ src/AppBundle/Service/UploadManager.php
@@ -10,7 +10,14 @@ class UploadManager
     {
         $client = new \Google_Client();
         $client->setApplicationName("MIWI");
-        $client->setAuthConfigFile('/var/www/v3/app/config/gcm.json');
+
+
+        $credential = new \Google_Auth_AssertionCredentials(
+            "202539044446-tfa6611ea0v491evalvpclarcos5822h@developer.gserviceaccount.com",
+            ['https://www.googleapis.com/auth/devstorage.read_write'],
+            file_get_contents("/var/www/v3/app/config/gcm.json")
+        );
+        $client->setAssertionCredentials($credential);
 
         $object = new \Google_Service_Storage_StorageObject();
         $storageService = new \Google_Service_Storage($client);
